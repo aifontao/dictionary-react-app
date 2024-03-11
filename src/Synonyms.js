d58d5a7928003a694f1a4782aa6df72e61cc38cf
aifontao <anaisabelfontao@gmail.com>
2024-03-11 20:09:22 +0000

    Rendered synonym component

---
+++ src/Synonyms.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  console.log(props.synonyms);
+  if (props.synonyms.length !== 0) {
+    return (
+      <div className="Synonyms">
+        <strong>Synonyms: </strong>
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <ul key={index}>
+              <li>{synonym}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
